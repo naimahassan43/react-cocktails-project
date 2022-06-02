13e8eda3ff61590924bddf85e2931d0cc0e40962
Naima Hassan <naima.hassan434@gmail.com>
2022-06-02 22:36:30 +0100

    About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eum
+        debitis atque exercitationem vel ea minima dignissimos, distinctio
+        assumenda a sunt eaque quis deserunt quia iste reiciendis fugiat
+        aspernatur voluptas mollitia. Impedit veritatis neque iste temporibus ex
+        iure fuga fugit.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
